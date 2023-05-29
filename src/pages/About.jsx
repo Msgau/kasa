a59de1f1fb124e1a79514e6a94270d1e7105ede6
Sebastien Gau <gausebastien@hotmail.fr>
2023-05-29 18:07:24 +0200

    Suppression d'1 composant ContentAbout dispensable

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,8 @@
 import Header from "../components/Header.js";
-import ContentAbout from "../components/ContentAbout.jsx";
+import "../css/ContentAbout.css";
+import React, { useState } from "react";
+import unroll from "../images/icons/VectorDown.png"
+import rollUp from "../images/icons/VectorUp.png";
 
 export default function About() {
     return (
@@ -8,4 +11,84 @@ export default function About() {
         <ContentAbout />
       </div>
     );
+}
+
+function ContentAbout() {
+  const [showFiabilite, setShowFiabilite] = useState(false);
+  const [payRespect, setShowRespect] = useState(false);
+  const [showService, setShowService] = useState(false);
+  const [showSecurite, setShowSecurite] = useState(false);
+
+  return (
+    <div className="about">
+      <div className="baniereAbout"></div>
+
+      <h2 onClick={() => setShowFiabilite(!showFiabilite)}>
+        Fiabilité
+        <img
+          src={showFiabilite ? rollUp : unroll}
+          alt="dérouler fiabilité"
+          className="arrowDown"
+        />
+      </h2>
+      {showFiabilite && (
+        <p className={`${showFiabilite ? 'visible' : 'invisible'}`}>
+          Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.
+        </p>
+      )}
+
+      <h2 onClick={() => setShowRespect(!payRespect)}>
+        Respect
+        <img
+          src={payRespect ? rollUp : unroll}
+          alt="dérouler respect"
+          className="arrowDown"
+        />
+      </h2>
+      {payRespect && (
+        <p className={`${payRespect ? 'visible' : 'invisible'}`}>
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </p>
+      )}
+
+      <h2 onClick={() => setShowService(!showService)}>
+        Service
+        <img
+          src={showService ? rollUp : unroll}
+          alt="dérouler service"
+          className="arrowDown"
+        />
+      </h2>
+      {showService && (
+        <p className={`${showService ? 'visible' : 'invisible'}`}>
+          Nos équipes se tiennent à votre disposition pour vous fournir une
+          expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+          moindre question.
+        </p>
+      )}
+
+      <h2 onClick={() => setShowSecurite(!showSecurite)}>
+        Sécurité
+        <img
+          src={showSecurite ? rollUp : unroll}
+          alt="dérouler sécurité"
+          className="arrowDown"
+        />
+      </h2>
+      {showSecurite && (
+        <p className={`${showSecurite ? 'visible' : 'invisible'}`}>
+          La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les
+          standards sont bien respectés. Nous organisons également des ateliers
+          sur la sécurité domestique pour nos hôtes.
+        </p>
+      )}
+    </div>
+  );
 }
